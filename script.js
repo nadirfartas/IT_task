2454d0d4d09f9360085224c5693462b3be45e2fb
nadirfartas <fartasnadir2003@gmail.com>
2024-09-09 00:10:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+const cards = document.querySelectorAll('.project-card');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate');
+        } else {
+            entry.target.classList.remove('animate');
+        }
+    });
+}, {
+    threshold: 0.5
+});
+
+cards.forEach(card => {
+    observer.observe(card);
+});
